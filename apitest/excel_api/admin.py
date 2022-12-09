0004a23af184f2691668f7213022ac3139c5246a
sobhan <sobhanmoghimi45@gmail.com>
2022-12-09 16:37:14 +0330

    Adding tables to AdminView

--- apitest/excel_api/admin.py
+++ apitest/excel_api/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from excel_api import models
 
+
+
+class ExcelDataAdmin(admin.ModelAdmin):
+    list_display = ('id', 'time', 'valid_action', 'symbol', 'total_position', 'initial_margin', 'total_deposit', 'order_price', 'stop_loss', 'take_profit', 'contract_type', 'token_quantity', 'leverage', 'liquidation_price')
+    list_display_links = ('id', 'time', 'valid_action', 'symbol', 'total_position', 'initial_margin', 'total_deposit', 'order_price', 'stop_loss', 'take_profit', 'contract_type', 'token_quantity', 'leverage', 'liquidation_price')
+    list_filter = ('time', 'valid_action', 'symbol', 'total_position', 'initial_margin', 'contract_type', 'token_quantity')
+    list_per_page = 20
 # Register your models here.
 admin.site.register(models.UserProfile)
-admin.site.register(models.ExcelData)
+admin.site.register(models.ExcelData, ExcelDataAdmin)
 admin.site.register(models.ConstantDatas)
