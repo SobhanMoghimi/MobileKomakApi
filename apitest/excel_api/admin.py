f0ec60d3169121f6f75397a801efa7dbcb075adc
sobhan <sobhanmoghimi45@gmail.com>
2022-12-10 11:38:10 +0330

    csv export the excel_model

--- apitest/excel_api/admin.py
+++ apitest/excel_api/admin.py
@@ -1,13 +1,46 @@
 from django.contrib import admin
 from excel_api import models
-
-
+from django.http import HttpResponse
+import csv
 
 class ExcelDataAdmin(admin.ModelAdmin):
-    list_display = ('id', 'time', 'valid_action', 'symbol', 'total_position', 'initial_margin', 'total_deposit', 'order_price', 'stop_loss', 'take_profit', 'contract_type', 'token_quantity', 'leverage', 'liquidation_price')
+    list_display = ('id', 'time',
+                    'valid_action',
+                    'symbol',
+                    'total_position',
+                    'initial_margin',
+                    'total_deposit',
+                    'order_price',
+                    'stop_loss',
+                    'take_profit',
+                    'contract_type',
+                    'token_quantity',
+                    'leverage',
+                    'liquidation_price')
     list_display_links = ('id', 'time', 'valid_action', 'symbol', 'total_position', 'initial_margin', 'total_deposit', 'order_price', 'stop_loss', 'take_profit', 'contract_type', 'token_quantity', 'leverage', 'liquidation_price')
-    list_filter = ('time', 'valid_action', 'symbol', 'total_position', 'initial_margin', 'contract_type', 'token_quantity')
+    list_filter = ('time',
+                   'valid_action',
+                   'symbol',
+                   'total_position',
+                   'initial_margin',
+                   'contract_type',
+                   'token_quantity')
     list_per_page = 20
+
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+        return response
+
+
 # Register your models here.
 admin.site.register(models.UserProfile)
 admin.site.register(models.ExcelData, ExcelDataAdmin)
